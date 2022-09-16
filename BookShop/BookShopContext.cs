c581761728c827453aa03e26b16ad8aaa94c808c
MaksMaksymiv99 <danio2620013@gmail.com>
2022-09-16 21:08:35 +0300

    Setap progect

---
+++ BookShop/BookShopContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace BookShop
+{
+    internal class BookShopContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseSqlServer();
+
+        }
+        protected override void OnModelCreating() { }
+    }
+}
