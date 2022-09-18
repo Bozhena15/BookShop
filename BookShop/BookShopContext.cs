c7133d7bc989804c620cec2214322f145cc64e3e
MaksMaksymiv99 <danio2620013@gmail.com>
2022-09-18 21:38:45 +0300

    Add Book Table and DbSet<Autor>

--- BookShop/BookShopContext.cs
+++ BookShop/BookShopContext.cs
@@ -2,13 +2,15 @@
 using Microsoft.SqlServer;
 using BookShop.EntityConfiguration;
 using BookShop.Models;
-
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace BookShop
 {
     public class BookShopContext:DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Autor> Autors { get; set; }
+        public DbSet<Book> Books { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
         {
             optionsBuilder.UseSqlServer("");
---
+++ BookShop/EntityConfiguration/BookConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using BookShop.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.EntityConfiguration
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.BookName).IsRequired();
+            builder.Property(x => x.GraduationYears);
+            builder.Property(x => x.BookPrice).IsRequired();
+            builder.Property(x => x.BookCost).IsRequired();
+            builder.Property(x => x.BookCount).IsRequired();
+            builder.Property(x => x.AutorId).IsRequired();
+            builder.Property(x => x.GenreId).IsRequired();
+            builder.Property(x => x.EditionId).IsRequired();
+        }
+    }
+}
