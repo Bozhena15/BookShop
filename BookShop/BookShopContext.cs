44a78b420df34ca05ae3608d6a9ace398270e6a9
zxcgodzilla <95091239+zxcgodzilla@users.noreply.github.com>
2022-09-19 22:00:09 +0300

    AddGenreTable

--- BookShop/BookShopContext.cs
+++ BookShop/BookShopContext.cs
@@ -11,10 +11,11 @@ namespace BookShop
         public DbSet<User> Users { get; set; }
         public DbSet<Autor> Autors { get; set; }
         public DbSet<Edition> Editions { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
         {
-            optionsBuilder.UseSqlServer("");
-
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BookShop;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -22,6 +23,7 @@ namespace BookShop
             modelBuilder.ApplyConfiguration(new UserConfiguration());
             modelBuilder.ApplyConfiguration(new AutorConfiguration());
             modelBuilder.ApplyConfiguration(new EditionConfiguration());
+            modelBuilder.ApplyConfiguration(new GenreConfiguration());
         }
     }
 }
---
+++ BookShop/EntityConfiguration/GenreConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookShop.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookShop.EntityConfiguration
+{
+    public class GenreConfiguration:IEntityTypeConfiguration<Genre>
+    {
+        public void Configure(EntityTypeBuilder<Genre> builder)
+        {
+            builder.HasKey(x=>x.Id);
+
+            builder.Property(x=>x.BookGenre);
+        }
+    }
+}
