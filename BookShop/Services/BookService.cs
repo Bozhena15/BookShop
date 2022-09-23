77b9e0131174436455124875bf2856535ff7b832
zxcgodzilla <95091239+zxcgodzilla@users.noreply.github.com>
2022-09-23 21:40:41 +0300

    BookService

---
+++ BookShop/Services/BookService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookShop.Models;
+
+namespace BookShop.Services
+{
+    public class BookService
+    {
+        private readonly BookShopContext _bookShopContext;
+
+        public BookService(BookShopContext bookShopContext)
+        {
+            _bookShopContext=bookShopContext;
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            var books = _bookShopContext.Books.ToList();
+            return books;
+        }
+
+        public void AddNewBooks(Book book)
+        {
+            if(book ==null)
+            {
+                throw new NullReferenceException(nameof(book));
+            }
+            _bookShopContext.Books.Add(book);
+            _bookShopContext.SaveChanges();
+        }
+
+        public void RemoveBooks(int id)
+        {
+            var book = _bookShopContext.Books.FirstOrDefault(x => x.Id == id);
+
+            if(book == null)
+            {
+                throw new NullReferenceException(nameof(book));
+            }
+            _bookShopContext.Books.Remove(book);
+            _bookShopContext.SaveChanges();
+        }
+    }
+}
