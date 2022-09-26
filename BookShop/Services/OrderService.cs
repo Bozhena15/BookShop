7d3bb99f7fb4a13695c2e16d781bc5009d8c7294
zxcgodzilla <95091239+zxcgodzilla@users.noreply.github.com>
2022-09-26 21:07:14 +0300

    OrderService

--- BookShop/Services/OrderService.cs
+++ BookShop/Services/OrderService.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookShop.Models;
 
 namespace BookShop.Services
 {
-    internal class OrderService
+    public class OrderService
     {
+        private readonly BookShopContext _bookShopContext;
+
+        public OrderService(BookShopContext bookShopContext)
+        {
+            _bookShopContext=bookShopContext;
+        }
+
+        public IEnumerable<Order> GetOrders()
+        {
+            var orders = _bookShopContext.Orders.ToList();
+            return orders;
+        }
+
+        public void AddNewOrders(Order order)
+        {
+            if (order == null)
+            {
+                throw new NullReferenceException(nameof(order));
+            }
+            _bookShopContext.Orders.Add(order);
+            _bookShopContext.SaveChanges();
+        }
+
+        public void RemoveOrders(int id)
+        {
+            var order = _bookShopContext.Orders.FirstOrDefault(x => x.Id == id);
+
+            if (order == null)
+            {
+                throw new NullReferenceException(nameof(order));
+            }
+            _bookShopContext.Orders.Remove(order);
+            _bookShopContext.SaveChanges();
+        }
     }
 }
